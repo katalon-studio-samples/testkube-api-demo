1782834741fc0a5b4613534c012e95c87299cf72
Coty Rosenblath <coty@pobox.com>
2023-05-24 16:11:51 -0400

    Add Home Test script and test case to API Test Suite
    Added a new script file with import statements for Katalon Core libraries and sent a request using Postman/Home object. Also added a new test case named "Home Test" to the project. Finally, added two test case links in the API Test Suite: one for the newly created "Home Test" test case and another for an existing test case.

--- Test Suites/API Test Suite.ts
+++ Test Suites/API Test Suite.ts
@@ -12,6 +12,13 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>b5d291c2-8613-4e59-8083-fc52cf9dba15</testSuiteGuid>
    <testCaseLink>
+      <guid>bd9de71e-47ac-4ed9-a925-3e3d816fe613</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
       <guid>ea962c87-129b-4e62-9884-e73306113f64</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
